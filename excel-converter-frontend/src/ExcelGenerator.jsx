8a331b7697464ad9ec6fb5e6324c9dbdce44301a
udayanu433 <151933256+udayanu433@users.noreply.github.com>
2026-01-01 15:54:14 +0530

    Update BACKEND_URL for ExcelGenerator component
    UDATATED URL

--- excel-converter-frontend/src/ExcelGenerator.jsx
+++ excel-converter-frontend/src/ExcelGenerator.jsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import './ExcelGenerator.css'; 
 
 // Replace this with your actual Render/Backend URL
-const BACKEND_URL = 'https://pdf-to-excelsheet-converter-api.onrender.com/generate-excel';
+const BACKEND_URL = 'https://uudayan450-ktu-resultsgpa-api.hf.space';
 
 export default function ExcelGenerator() {
   const { year, semester } = useParams(); // Retrieves scheme (2019/2024) and semester (S1-S8)
